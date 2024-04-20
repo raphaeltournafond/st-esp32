bf5678646b4e880677f417f755441667c1ee6bac
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-20 11:11:03 +0200

    feat: add BLE capability

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,52 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_MMA8451.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-#define MMA8451_DEFAULT_ADDRESS (0x1D) /**< MMA8451 default I2C address */
+#define MMA8451_DEFAULT_ADDRESS (0x1D) // MMA8451 default I2C address
+
+#define SERVICE_UUID        "dc23fd97-88af-47ff-98a6-4fc529b66d7b"
+#define CHARACTERISTIC_UUID "6a4496fd-51b3-419e-ac53-956fdbf0658a"
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
+BLECharacteristic *pCharacteristic;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {
     delay(10); // Wait for serial monitor
   }
+
+  // Bluetooth Setup
+
+  Serial.println("Starting BLE setup!");
+
+  BLEDevice::init("Smart Tracker BLE");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE |
+                                         BLECharacteristic::PROPERTY_NOTIFY
+                                       );
+
+  pCharacteristic->setValue("Hello World!");
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+  Serial.println("BLE setup done");
+
+  // I2C Accelerometer setup
+
+  Serial.println("Starting accelerometer connection");
   
   Wire.begin();
 
@@ -21,6 +57,8 @@ void setup() {
   }
 
   mma.setRange(MMA8451_RANGE_4_G);
+
+  Serial.println("Accelerometer connection established");
 }
 
 void loop() {
@@ -32,5 +70,5 @@ void loop() {
   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
   Serial.print("Z: "); Serial.println(event.acceleration.z);
 
-  delay(500); // Delay between readings
+  delay(1000); // Delay between readings
 }
